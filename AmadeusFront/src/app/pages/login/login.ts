4657033757dc347327094f32a6a81c4ac0c2bfbf
Stéphane Biehler <biehler@s2i-evolution.eu>
2026-01-12 18:37:36 +0100

    ✨ Fixed login fully
    Fixed login fully

--- AmadeusFront/src/app/pages/login/login.ts
+++ AmadeusFront/src/app/pages/login/login.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../services/AuthService';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
 
+  authService = inject(AuthService);
+  loginProgress: boolean = false;
+
+  protected loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  });
+
+  onSubmit(){
+    if (this.loginForm.valid) {
+      this.loginProgress = true;
+      this.authService.login(this.loginForm.value.email!, this.loginForm.value.password!);
+    }else{
+      console.error('Form is invalid');
+      console.log(this.loginForm.value);
+    }
+  }
 }
